90de7b1721dd03fa74a7b7fe96e80e8b88972391
khu-twilio <82540340+khu-twilio@users.noreply.github.com>
2022-06-04 23:56:23 -0700

    updated workers under supervisor

--- plugin-complete-associate-task/src/util/getAssociateTasks.js
+++ plugin-complete-associate-task/src/util/getAssociateTasks.js
@@ -3,13 +3,27 @@ import { endTaskUtil } from "./endTask";
 
 class GetAssociateTask {
   loopTask() {
-    const workerTasks = manager.store.getState()?.flex?.worker?.tasks || [];
+    const selectedWorkerSid =
+      manager.store.getState()?.flex?.view?.selectedWorkerInSupervisorSid || [];
+
+    const workerTasks = this.getWorkerTasks(selectedWorkerSid);
     // eslint-disable-next-line no-unused-vars
     workerTasks.forEach(function (value, key) {
       if (value.status == "wrapping") {
         endTaskUtil.startEndTask(value.sid, value.taskSid);
       }
     });
+  }
+
+  getWorkerTasks(selectedWorkerSid) {
+    const workers = manager.store.getState()?.flex?.supervisor?.workers || [];
+    var workerTasks;
+    workers.forEach(function (value) {
+      if (value.worker.sid == selectedWorkerSid) {
+        workerTasks = value.tasks;
+      }
+    });
+    return workerTasks;
   }
 }
 
